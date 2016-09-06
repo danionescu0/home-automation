e665c79f829b248f85173deed63cddfb71fb5dd2
Dan Ionescu <Dan Ionescu@localhost>
2016-09-06 23:33:04 +0300

    developing ifttt parser

--- python-server/ifttt/parser/Token.py
+++ python-server/ifttt/parser/Token.py
@@ -1,10 +1,12 @@
 class Token:
     TYPE_BOOLEAN_AND = 'and'
-    TYPE_BOOLEAN_OR = 'and'
+    TYPE_BOOLEAN_OR = 'or'
     TYPE_EXPR_EQUAL = 'eq'
     TYPE_EXPR_GREATER = 'gt'
+    TYPE_EXPR_LESS = 'lt'
     TYPE_LITERAL = 'literal'
     TYPE_SENSOR = 'sensor'
+    TYPE_TIME = 'time'
     TYPE_ACTUATOR = 'actuator'
 
     def __init__(self, type, value):
