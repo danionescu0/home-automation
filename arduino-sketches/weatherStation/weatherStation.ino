b911804237334e6f34bc9feeaf4a3b38dcf9c77c
Dan Ionescu <Dan Ionescu@localhost>
2016-10-03 10:49:49 +0300

    added voltage logging for the weather station

--- arduino-sketches/weatherStation/weatherStation.ino
+++ arduino-sketches/weatherStation/weatherStation.ino
@@ -24,6 +24,7 @@ struct sensorData
       byte temperature;
       byte rain;
       int  pressure;
+      long voltage;
       byte light;
   };
 
@@ -47,7 +48,7 @@ void setup()
 
 void loop() 
 {   
-    deepSleep(7);    
+    deepSleep(75);    
     updateSenzors();
     transmitData();
 }
@@ -59,6 +60,7 @@ void updateSenzors()
     sensors.temperature = bme.readTemperature();
     sensors.pressure = bme.readPressure() / 100.0F;
     sensors.humidity = bme.readHumidity();
+    sensors.voltage = readVcc();
 }
 
 void transmitData()
@@ -67,9 +69,11 @@ void transmitData()
     Serial.print("Temp:");Serial.println(sensors.temperature);
     Serial.print("Humid:");Serial.println(sensors.humidity);
     Serial.print("Pressure:");Serial.println(sensors.pressure);
+    Serial.print("Voltage:");Serial.println(sensors.voltage);
     transmitSenzorData("T", sensors.temperature);
     transmitSenzorData("H", sensors.humidity);
     transmitSenzorData("PS", sensors.pressure);
+    transmitSenzorData("V", sensors.voltage);
 }
 
 void emptyIncommingSerialBuffer()
@@ -98,4 +102,31 @@ void deepSleep(int eightSecondCycles)
     }
     digitalWrite(peripherialsPowerPin, HIGH);
     delay(500);
+}
+
+long readVcc() {
+    // Read 1.1V reference against AVcc
+    // set the reference to Vcc and the measurement to the internal 1.1V reference
+    #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+      ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+      ADMUX = _BV(MUX5) | _BV(MUX0);
+    #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+      ADMUX = _BV(MUX3) | _BV(MUX2);
+    #else
+      ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    #endif  
+  
+    delay(2); // Wait for Vref to settle
+    ADCSRA |= _BV(ADSC); // Start conversion
+    while (bit_is_set(ADCSRA,ADSC)); // measuring
+  
+    uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+    uint8_t high = ADCH; // unlocks both
+  
+    long result = (high<<8) | low;
+  
+    result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+    
+    return result; // Vcc in millivolts
 }
