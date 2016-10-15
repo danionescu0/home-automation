a7b1867a906aeaf200389c55eaa0224fa97a053c
Dan Ionescu <Dan Ionescu@localhost>
2016-10-15 19:55:42 +0300

    updated  meteo station to work report light levels

--- arduino-sketches/weatherStation/weatherStation.ino
+++ arduino-sketches/weatherStation/weatherStation.ino
@@ -10,9 +10,11 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <BH1750.h>
 
-SoftwareSerial serialComm(4, 3); // RX, TX
+SoftwareSerial serialComm(4, 5); // RX, TX
 Adafruit_BME280 bme; 
+BH1750 lightMeter;
 
 byte sensorsCode = 1;
 const byte peripherialsPowerPin = 6;
@@ -56,10 +58,12 @@ void loop()
 void updateSenzors() 
 {
     bme.begin();
-    delay(100);
+    lightMeter.begin();
+    delay(300);
     sensors.temperature = bme.readTemperature();
     sensors.pressure = bme.readPressure() / 100.0F;
     sensors.humidity = bme.readHumidity();
+    sensors.light = lightMeter.readLightLevel();
     sensors.voltage = readVcc();
 }
 
@@ -69,10 +73,12 @@ void transmitData()
     Serial.print("Temp:");Serial.println(sensors.temperature);
     Serial.print("Humid:");Serial.println(sensors.humidity);
     Serial.print("Pressure:");Serial.println(sensors.pressure);
+    Serial.print("Light:");Serial.println(sensors.light);
     Serial.print("Voltage:");Serial.println(sensors.voltage);
     transmitSenzorData("T", sensors.temperature);
     transmitSenzorData("H", sensors.humidity);
     transmitSenzorData("PS", sensors.pressure);
+    transmitSenzorData("L", sensors.light);
     transmitSenzorData("V", sensors.voltage);
 }
 
@@ -91,7 +97,7 @@ void transmitSenzorData(String type, int value)
     serialComm.print(":");
     serialComm.print(value);
     serialComm.print("|");
-    delay(500);
+    delay(200);
 }
 
 void deepSleep(int eightSecondCycles)
@@ -120,12 +126,9 @@ long readVcc() {
     delay(2); // Wait for Vref to settle
     ADCSRA |= _BV(ADSC); // Start conversion
     while (bit_is_set(ADCSRA,ADSC)); // measuring
-  
     uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
     uint8_t high = ADCH; // unlocks both
-  
     long result = (high<<8) | low;
-  
     result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
     
     return result; // Vcc in millivolts
