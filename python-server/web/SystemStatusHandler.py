b423fb3d265f52bdb00f8e91ae558d1ec3c421b8
Dan Ionescu <dan.ionescu@machteamsoft.ro>
2016-10-31 20:48:20 +0200

    refactored staatus page, added a table, added minutes since updated and a sorted by minutes

--- python-server/web/SystemStatusHandler.py
+++ python-server/web/SystemStatusHandler.py
@@ -1,7 +1,9 @@
 from tornado.web import  authenticated
-from datetime import datetime, timedelta
+from datetime import datetime
+from operator import itemgetter
 from dateutil import tz
 from web.BaseHandler import BaseHandler
+
 
 class SystemStatusHandler(BaseHandler):
     def initialize(self,  sensors_repo):
@@ -19,16 +21,19 @@ class SystemStatusHandler(BaseHandler):
         data = self.__sensors_repo.get_sensors()
         from_zone = tz.gettz('UTC')
         to_zone = tz.gettz('Europe/Bucharest')
+        current_time = datetime.now(to_zone)
         sensors_list = []
 
         for datapoint in data:
-            initial_date = datetime.fromtimestamp(int(datapoint['last_updated'])).replace(tzinfo=from_zone)
+            initial_date = datetime.fromtimestamp(int(datapoint['last_updated']))\
+                .replace(tzinfo=from_zone)
+
             local_date = initial_date.astimezone(to_zone)
             datetime_text = local_date.strftime('%Y-%m-%d %H:%M:%S')
             sensors_list.append({
                 'name': "{0}_{1}".format(datapoint['type'], datapoint['location']),
-                'last_updated' : datetime_text
+                'last_updated' : datetime_text,
+                'minutes_since_updated' : (current_time - local_date).seconds / 60
             })
-        print sensors_list
 
-        return sensors_list
+        return sorted(sensors_list, key=itemgetter('minutes_since_updated'), reverse=True)
