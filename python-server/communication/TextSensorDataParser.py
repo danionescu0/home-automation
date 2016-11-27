0fc76780c6880cffc6a1cf88629794e0fcc8cecc
Dan Ionescu <ionescu.dan84@gmail.com>
2016-11-27 21:01:38 +0200

    refactored wemo switch integration, renamed things more appropriate

---
+++ python-server/communication/TextSensorDataParser.py
@@ -0,0 +1,39 @@
+import re
+
+from communication.SensorsParseException import SensorsParseException
+
+class TextSensorDataParser:
+    SENSOR_REGEX = '([A-Z]{1,2})(\d{1,2})?\:(\d{1,4})'
+
+    def __init__(self, sensors_conf):
+        self.__sensors_conf = sensors_conf
+        self.__sensor_separator = '|'
+
+    def is_buffer_parsable(self, buffer):
+        return buffer.endswith(self.__sensor_separator)
+
+    def parse(self, text_buffer):
+        text_buffer = text_buffer[:-1]
+        sensors = []
+        for sensor in text_buffer.split('|'):
+            sensor_components = re.findall(self.SENSOR_REGEX, sensor)
+            if not sensor_components:
+                raise SensorsParseException('Cannot parse string:{0}'.format(sensor))
+            sensors.append(self.__get_sensor(sensor_components[0]))
+
+        return sensors
+
+    def __get_sensor(self, sensor_components):
+        code = sensor_components[0]
+        if sensor_components[1] == '':
+            location = False
+        else:
+            location = sensor_components[1]
+        value = sensor_components[2]
+        for sensor in self.__sensors_conf:
+            if sensor['communication_code'][0] == code and sensor['communication_code'][1] == location:
+                found_sensor = sensor
+                found_sensor['value'] = int(value)
+                return found_sensor
+
+        raise SensorsParseException('Sensor with code: {0} and location {1} not found!'.format(code, location))
---
+++ python-server/communication/WemoSwitch.py
@@ -0,0 +1,28 @@
+from Base import Base
+from ouimeaux.environment import Environment
+from ouimeaux.environment import UnknownDevice
+
+class WemoSwitch(Base):
+    def send(self, which, value):
+        env = Environment()
+        try:
+            env.start()
+            env.discover(seconds=1)
+            switch = env.get_switch(which)
+            if (value):
+                switch.on()
+            else:
+                switch.off()
+        except UnknownDevice:
+            return False
+
+        return True
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def listen(self, complete_message_callback, receive_message_callback):
+        pass
