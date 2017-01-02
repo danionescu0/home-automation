d840c49130cad8b8e5da0130113a9fb288c65dbe
Dan Ionescu <ionescu.dan84@gmail.com>
2017-01-02 16:45:00 +0200

    modified DC courtains sketch to work with two relays and a power tranzistor

--- arduino-sketches/DCMotorCourtains/DCMotorCourtains.ino
+++ arduino-sketches/DCMotorCourtains/DCMotorCourtains.ino
@@ -1,69 +1,68 @@
 #include <SoftwareSerial.h>
 
-const int courtain1dirPin1 = 5;
-const int courtain1dirPin2 = 6;
-const int courtain2dirPin1 = 7;
-const int courtain2dirPin2 = 8;
-const int courtain1EnablePin = 3;
-const int courtain2EnablePin = 9;
-const unsigned long onState = 1;
-const unsigned long offState = 0;
-const unsigned long changeStateMillis = 80000;
-const int courtain1PWM = 120;
-const int courtain2PWM = 120;
+const byte pwmPower = 160;
+const byte powerPwmPin = 9;
+const byte relay1StatePin = 8;
+const byte relay2StatePin = 7;
 
 SoftwareSerial bluetooth(10, 11); // RX, TX
+char buffer[] = {' ',' ',' '};
 
 void setup() 
 {
     Serial.begin(9600);
     bluetooth.begin(9600);
-    pinMode(courtain1dirPin1, OUTPUT);
-    pinMode(courtain1dirPin2 , OUTPUT);
-    pinMode(courtain1EnablePin, OUTPUT);
-    digitalWrite(courtain1dirPin1, HIGH);
-    digitalWrite(courtain1dirPin2, LOW);
-    digitalWrite(courtain1EnablePin, LOW);
-    pinMode(courtain2dirPin1, OUTPUT);
-    pinMode(courtain2dirPin2 , OUTPUT);
-    pinMode(courtain2EnablePin, OUTPUT);
-    digitalWrite(courtain2dirPin1, HIGH);
-    digitalWrite(courtain2dirPin2, LOW);
-    digitalWrite(courtain2EnablePin, LOW);   
-    Serial.println("done with init");
-    bluetooth.println("test");
+    pinMode(powerPwmPin, OUTPUT);
+    pinMode(relay1StatePin , OUTPUT);
+    pinMode(relay2StatePin, OUTPUT);
+    digitalWrite(powerPwmPin, LOW);
+    digitalWrite(relay1StatePin, LOW);
+    digitalWrite(relay2StatePin, LOW);   
 }
 
 void loop() 
 {
   if (bluetooth.available() > 0) {   
-      int received = bluetooth.read() - 48;
-      Serial.println(received);   
-      delay(100);
-      toggleState(received);  
+      bluetooth.readBytesUntil(';', buffer, 4);
+      toggleState(getState(), getDuration());
+      clearBuffer();  
   } 
 }
 
-void toggleState(int received)
+boolean getState()
 {
-    if (received == offState) {
-        Serial.println("on");
-        digitalWrite(courtain1dirPin1, HIGH);
-        digitalWrite(courtain1dirPin2, LOW);   
-        digitalWrite(courtain2dirPin1, HIGH);
-        digitalWrite(courtain2dirPin2, LOW);
-        analogWrite(courtain1EnablePin, courtain1PWM);
-        analogWrite(courtain2EnablePin, courtain2PWM);                  
-    } else if (received == onState) {
-        Serial.println("off");
-        digitalWrite(courtain1dirPin1, LOW);
-        digitalWrite(courtain1dirPin2, HIGH);   
-        digitalWrite(courtain2dirPin1, LOW);
-        digitalWrite(courtain2dirPin2, HIGH);
-        analogWrite(courtain1EnablePin, courtain1PWM);
-        analogWrite(courtain2EnablePin, courtain2PWM);                            
-    } 
-    delay(changeStateMillis);    
-    digitalWrite(courtain1EnablePin, LOW);    
-    digitalWrite(courtain2EnablePin, LOW);    
+    return buffer[0] == 'C' ? false : true;
 }
+
+byte getDuration()
+{
+    String value = "";
+    value += String(buffer[1]) + String(buffer[2]);
+
+    return (byte) value.toInt();
+}
+
+void toggleState(boolean state, byte duration)
+{  
+    Serial.print("State:");Serial.println(state);
+    Serial.print("Duration:");Serial.println(duration);
+    if (state) {
+        digitalWrite(relay1StatePin, HIGH);
+        digitalWrite(relay2StatePin, HIGH);        
+    } else {
+        digitalWrite(relay1StatePin, LOW);
+        digitalWrite(relay2StatePin, LOW);        
+    }
+    analogWrite(powerPwmPin, pwmPower);
+    long millisecondsSleep = (long) duration * 1000;
+    delay(millisecondsSleep);
+    digitalWrite(powerPwmPin, LOW);
+}
+
+void clearBuffer()
+{
+    for (int i=0; i<=2; i++) {
+        buffer[i] = ' ';
+    }
+}
+
