4fdced3806b74b932e088f30627d97eeac9af812
Dan Ionescu <ionescu.dan84@gmail.com>
2017-06-04 19:26:59 +0300

    implemented text to speech as an external api to a different device, refactored code

--- python-server/sound/RemoteSpeaker.py
+++ python-server/sound/RemoteSpeaker.py
@@ -1,7 +1,9 @@
 import subprocess
+from sound.SoundApi import SoundApi
+
 from typeguard import typechecked
 
-class TextToSpeech:
+class TextToSpeech(SoundApi):
     @typechecked()
     def say(self, text: str, nr_times = 1) -> None:
         [self.__say_once(text) for i in range(0, nr_times)]
